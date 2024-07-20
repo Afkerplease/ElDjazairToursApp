8f717520118cd2bfda3d8b8fcbdecf8a481084b0
afker please <afker2020@gmail.com>
2024-07-20 15:44:03 +0200

    popularSection content is done needs more styling for later

--- front-end/src/components/PopularSection/PopularSection.jsx
+++ front-end/src/components/PopularSection/PopularSection.jsx
@@ -1,7 +1,39 @@
 import React from "react";
-
+import "./popularSection.scss";
+import image1 from "../../images/place1.jpeg";
+import image2 from "../../images/place2.jpg";
+import image3 from "../../images/place3.jpg";
 function PopularSection() {
-  return <div>PopularSection</div>;
+  return (
+    <section className=" popular__section">
+      <div className="popular__header">
+        <h2>Popular things to do</h2>
+        <span>See all</span>
+      </div>
+      <div className="popular__images">
+        {/* image */}
+        <div className=" popular__image__container">
+          <img className="popular__image" src={image1} alt="" />
+          <p>dummy data</p>
+        </div>
+        {/* image */}
+        <div className=" popular__image__container">
+          <img className="popular__image" src={image2} alt="" />
+          <p>dummy data</p>
+        </div>
+        {/* image */}
+        <div className=" popular__image__container">
+          <img className="popular__image" src={image3} alt="" />
+          <p>dummy data</p>
+        </div>
+        {/* image */}
+        <div className=" popular__image__container">
+          <img className="popular__image" src={image3} alt="" />
+          <p>dummy data</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default PopularSection;
--- front-end/src/pages/Home/HomePage.jsx
+++ front-end/src/pages/Home/HomePage.jsx
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 import "./homepage.scss";
 import React, { useState, useEffect } from "react";
 import HeroSection from "../../components/HeroSection/HeroSection";
+import PopularSection from "../../components/PopularSection/PopularSection";
 
 function HomePage() {
   return (
     <>
-      <HeroSection />
-      {/* <PopularSection />
-      <ChooseSection />
-      <TrendingDistinationsSection />
-      <CustomerSection /> */}
+      {/* <HeroSection /> */}
+      <PopularSection />
+      {/* <ChooseSection /> */}
+      {/* <TrendingDistinationsSection /> */}
+      {/* <CustomerSection /> */}
     </>
   );
 }
