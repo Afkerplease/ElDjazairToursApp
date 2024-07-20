db3440a0f1da8f6690ca86ae3d612be7ea4eff94
afker please <afker2020@gmail.com>
2024-07-20 16:49:45 +0200

    Choose section content done needs more styles

--- front-end/src/components/ChooseSection/ChooseSection.jsx
+++ front-end/src/components/ChooseSection/ChooseSection.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import ticket from "../../images/ticket.svg fill.png";
+import vector from "../../images/Vector.png";
+import medal from "../../images/medal.svg.png";
+import diamond from "../../images/diamond.svg fill.png";
+import "./chooseSection.scss";
 
 function ChooseSection() {
-  return <div>ChooseSection</div>;
+  return (
+    <section className="choose__section">
+      <div className="choose__container">
+        <h2>Why choose ElDjazairTours</h2>
+        <div className="cards__container">
+          {/* card */}
+          <div className="card">
+            <img className="card__image" src={ticket} alt="" />
+            <h3 className="card__title">Ultimate flexibility</h3>
+            <p className="card__text">
+              You're in control with free cancellation
+            </p>
+          </div>
+          {/* card */}
+          <div className="card">
+            <img className="card__image" src={vector} alt="" />
+            <h3 className="card__title">Memorable experiences </h3>
+            <p className="card__text">
+              Browse and book tours and activities so incredible,you'll want to
+              tell your freinds
+            </p>
+          </div>
+          {/* card */}
+          <div className="card">
+            <img className="card__image" src={diamond} alt="" />
+            <h3 className="card__title">Quality at our core</h3>
+            <p className="card__text">
+              High-quality standars.Millions of reviews. Atourz company
+            </p>
+          </div>
+          {/* card */}
+          <div className="card">
+            <img className="card__image" src={medal} alt="" />
+            <h3 className="card__title">Award-winning support</h3>
+            <p className="card__text">
+              New price? New plan? No problem. We're here to help,24/7.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ChooseSection;
--- front-end/src/pages/Home/HomePage.jsx
+++ front-end/src/pages/Home/HomePage.jsx
@@ -3,13 +3,14 @@ import "./homepage.scss";
 import React, { useState, useEffect } from "react";
 import HeroSection from "../../components/HeroSection/HeroSection";
 import PopularSection from "../../components/PopularSection/PopularSection";
+import ChooseSection from "../../components/ChooseSection/ChooseSection";
 
 function HomePage() {
   return (
     <>
-      {/* <HeroSection /> */}
+      <HeroSection />
       <PopularSection />
-      {/* <ChooseSection /> */}
+      <ChooseSection />
       {/* <TrendingDistinationsSection /> */}
       {/* <CustomerSection /> */}
     </>
