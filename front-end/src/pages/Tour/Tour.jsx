d6c7e00b7b1d9127ea1e49e074b454351d587b32
afker please <afker2020@gmail.com>
2024-07-24 20:13:10 +0200

    comments are working

--- front-end/src/pages/Tour/Tour.jsx
+++ front-end/src/pages/Tour/Tour.jsx
@@ -1,25 +1,36 @@
 import React, { useState, useEffect } from "react";
 import "./Tour.scss";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Tour = () => {
   const [newComment, setNewComment] = useState("");
   const [comments, setComments] = useState([]);
   const [tourData, setTourData] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
 
   const tourid = useParams();
-  console.log(tourid.id);
 
+  // !! function to get the tour data
   async function getTourData() {
     const res = await fetch(`/api/v1/tours/${tourid.id}`);
     const data = await res.json();
-    console.log(data);
 
     setTourData(data.data.tour);
   }
-  console.log(tourData);
+  async function getTourComment() {
+    const res = await fetch(`/api/v1/reviews/${tourid.id}`);
+    const data = await res.json();
+    console.log(data.data);
+
+    setComments(data.data);
+  }
+  console.log(comments);
+
   useEffect(() => {
     getTourData();
+    getTourComment();
   }, []);
 
   const addComment = () => {
@@ -55,9 +66,10 @@ const Tour = () => {
           <button onClick={addComment}>Add Comment</button>
         </div>
         <ul className="comments-list">
+          {/* <li className="comment">{comments[0].comment}</li> */}
           {comments.map((comment, index) => (
             <li key={index} className="comment">
-              {comment}
+              {comment.comment}
               <button onClick={() => deleteComment(index)}>Delete</button>
             </li>
           ))}
